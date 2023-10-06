ab2635f60fddddb2f8987c40e696079f63e34a74
tuposilwe <rudigermkondya@gmail.com>
2023-10-06 06:55:22 -0700

    Optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard/ProductCard'
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
+import Image from "next/image";
+import feynman from '@/public/images/feynman.png';
 
 export default async function Home() {
- const session =await getServerSession(authOptions);
-
   return (
-   <main>
-    <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
-    <Link href='/users'>Users</Link>
-    <ProductCard/>
+   <main className="relative h-screen">
+     <Image
+      src="https://bit.ly/react-cover"  
+      alt="feyman"
+      fill
+      className="object-cover"
+      sizes="(max-width:480px) 100vw, (max-width:768px) 50vw,33vw"
+      quality={100}
+      priority
+      />
     </main>
   )
 }
