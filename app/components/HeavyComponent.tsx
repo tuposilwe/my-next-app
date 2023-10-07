d7d44f61af59186bcf1c3d8a42e1a0bbf23a4ab6
tuposilwe <rudigermkondya@gmail.com>
2023-10-06 22:29:46 -0700

    heavy component

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeavyComponent() {
+  return (
+    <div>My Heavy Component</div>
+  )
+}
+
+export default HeavyComponent
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image";
-import feynman from '@/public/images/feynman.png';
+'use client';
 
-export default async function Home() {
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import('./components/HeavyComponent'),
+{
+  ssr:false,
+  loading: () => <p>Loading...</p>
+  
+});
+
+export default function Home() {
+ const [isVisible,setVisible] = useState(false);
+
   return (
    <main className="relative h-screen">
     <h1>Hello World</h1>
-     <Image
-      src="https://bit.ly/react-cover"  
-      alt="feyman"
-      fill
-      className="object-cover"
-      sizes="(max-width:480px) 100vw, (max-width:768px) 50vw,33vw"
-      quality={100}
-      priority
-      />
+    <button onClick={() => setVisible(!isVisible )}>Show</button>
+    {isVisible && <HeavyComponent/>}
     </main>
   )
 }
